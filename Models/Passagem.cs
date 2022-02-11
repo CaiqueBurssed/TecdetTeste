d05f5c992ffd02d8ac8125e818577d115775edbb
CaiqueBurssed <caique.burssed@gmail.com>
2022-02-11 15:49:45 -0300

    added model class

---
+++ Models/Passagem.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TecdetTeste.Models
+{
+    class Passagem
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Placa { get; set; }
+
+        [Required]
+        public DateTime Data { get; set; }
+
+        [Required]
+        public DateTime Hora { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Equipamento { get; set; }
+
+        [Required]
+        [MaxLength(250)]
+        public string Faixa { get; set; }
+    }
+}
